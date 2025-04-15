17fb8be505ef283d17f87e093a720cf3317525cb
sonunagose <sonunagose10@gmail.com>
2025-04-15 11:43:10 +0530

    Add files via upload

---
+++ SQL/Is null & Is Not Null(15-04-2025).sql
@@ -0,0 +1,15 @@
+select * from cricketer;
+select * from student;
+select * from User;
+
+#### IS NULL:=
+select * from cricketer;
+Select age from cricketer where age is null;
+select email from student where email is null;
+
+#### IS NOT NULL:=
+select * from cricketer;
+Select id from cricketer where id is not null;
+select * from student_information;
+Select Gender from student_information where Gender is not null;
+
