20041130c35669241f7ab4401528ad3516aa9ba1
sonunagose <sonunagose10@gmail.com>
2025-04-16 11:15:41 +0530

    Add files via upload

---
+++ SQL/Airthmatic Operation query(16-04-2025).sql
@@ -0,0 +1,40 @@
+Select * from student_information;
+
+#### MIN QUERY:=
+select min(GPA) from student_information;
+
+#### AVERAGE QUERY:=
+select avg(GPA) from student_information;
+
+#### DIVISION QUERY:=
+Select age DIV GPA from student_information;
+
+#### COUNT QUERY:=
+select count(GPA) from student_information;
+select count(Age) from student_information;
+select count(Department) from student_information;
+
+#### SUM QUERY:=
+select Sum(GPA) from student_information;
+select Sum(Age) from student_information;
+
+#### ABS QUERY:=
+select ABS(-2000);
+
+#### MAX QUERY:=
+select MAX(GPA) from student_information;
+select MAX(age) from student_information;
+select * from enrollments;
+
+#### ROUND QUERY:=
+select ROUND(GPA,2) from student_information;
+select ROUND(GPA,4) from student_information;
+
+#### SORT QUERY:=
+select sqrt(Department) from student_information;
+select sqrt(age) from student_information;
+select sqrt(GPA) from student_information;
+
+#### LOG QUERY:=
+select log(GPA) from student_information;
+select log(Age) from student_information;
